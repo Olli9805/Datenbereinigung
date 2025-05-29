8587fc79bc580b62a41ab59e0abc7e49990af246
Lennert Wollmann <lennert.wollmann@gmail.com>
2025-05-29 19:16:46 +0200

    Mit kombinierter csv

--- lennert_test.py
+++ lennert_test.py
@@ -44,12 +44,31 @@ def bereinigen(df: pd.DataFrame, spalte: str, save_path: str = None) -> pd.DataF
     vergleich = df[['MESS_DATUM', spalte, 'Mittelwert',
                     'Median', 'Inter_Linear', 'ffill', 'bfill']]
 
+    rückgabe_df = pd.DataFrame({
+        'MESS_DATUM': df['MESS_DATUM'],
+        spalte: df[spalte],
+        'mit_nan': df['mit_nan'],
+        'Inter_Linear': linear,
+        'Mittelwert': df['Mittelwert'],
+        'Median': df['Median'],
+        'ffill': df['ffill'],
+        'bfill': df['bfill']
+    })
+
     if save_path:
         vergleich.to_csv(save_path, index=False, sep=';')
-    return vergleich
+    return rückgabe_df
 
 
 df = pd.read_csv("Datenbereinigung/data/daten2.txt", sep=";")
 df['MESS_DATUM'] = pd.to_datetime(df['MESS_DATUM'].astype(str), format="%Y%m%d%H")
-bereinigen(df,'TT_TER', save_path="temp.csv")
-bereinigen(df, 'RF_TER', save_path="hum.csv")
+
+temp_df = bereinigen(df, 'TT_TER', save_path="temp.csv")
+hum_df = bereinigen(df, 'RF_TER', save_path="hum.csv")
+
+df['TT_TER_bereinigt'] = temp_df['Inter_Linear'].combine_first(temp_df['mit_nan'])
+df['RF_TER_bereinigt'] = hum_df['Inter_Linear'].combine_first(hum_df['mit_nan'])
+
+export_df = df[['STATIONS_ID', 'MESS_DATUM', 'QN_4', 'TT_TER_bereinigt', 'RF_TER_bereinigt', 'eor']]
+export_df.columns = ['STATIONS_ID', 'MESS_DATUM', 'QN_4', 'TT_TER', 'RF_TER', 'eor']
+export_df.to_csv("bereinigte_werte.csv", index=False, sep=';')
